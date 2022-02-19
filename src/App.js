6a44acd239e2d2546549ad2db2ff224d58ba7286
Niranjan Devasani <niranjan.devasani@gmail.com>
2022-02-19 10:44:15 +0530

    Layout component added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import Timeline from "./components/timeline/Timeline";
 import Navbar from "./components/nav-bar/Navbar";
 import Profile from "./components/profile/Profile";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <div className="app">
-      <Navbar/>
-      <Profile/>
+      <Layout />
     </div>
   );
 }
---
+++ src/components/layout/Layout.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Layout.css';
+
+const Layout = () => {
+    return (
+        <div className="page">
+            <div className="header">
+
+            </div>
+            <div className="body">
+                <div className="menu">
+                    <div className="item">Link 1</div>
+                    <div className="item">Link 2</div>
+                    <div className="item">Link 3</div>
+                </div>
+                <div className="content">
+
+                </div>
+            </div>
+            <div className="footer">
+
+            </div>
+        </div>
+    )
+};
+
+export default Layout;
